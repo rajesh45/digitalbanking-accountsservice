6e4272ddf44c431192cbe4f262a8f2a46ac88ab0
Mahesh Natarajan <umamags@gmail.com>
2017-05-25 11:41:06 +0530

    added war support

--- src/main/java/com/capg/accservices/Application.java
+++ src/main/java/com/capg/accservices/Application.java
@@ -1,23 +1,29 @@
 package com.capg.accservices;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 //import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-
-import org.apache.log4j.Logger;
 
 @SpringBootApplication
 @ComponentScan("com.capg.accservices")
 /**
- * This is the main spring boot application 
+ * This is the main spring boot application
+ * 
  * @author mahenata
  *
  */
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	private static Logger log = Logger.getLogger(Application.class);
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(final String[] args) throws Throwable {
 		System.out.println("ENTERING MAIN METHOD");
